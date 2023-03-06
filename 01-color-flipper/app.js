1c34f428af5236dd21eba84eaeb9cb0e26c12e0b
Tanya <taniyhamail@gmail.ua>
2023-03-06 17:06:07 +0200

    added simple color changing

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+  //get random number 0 - 3
+  const randomNumber = getRandomeNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomeNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
