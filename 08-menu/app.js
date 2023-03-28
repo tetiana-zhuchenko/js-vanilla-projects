0847c27b14d8a4f5cfe1b619256297ae59f95ee9
Tanya <taniyhamail@gmail.ua>
2023-03-28 20:28:06 +0300

    08-menu/separated 'dynamic buttons' code to own function/finished project 08-menu

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -88,7 +88,29 @@ const container = document.querySelector('.btn-container');
 // load items
 window.addEventListener('DOMContentLoaded', function () {
   displayMenuItems(menu);
+  displayMenuButtons();
+});
 
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return ` <article class="menu-item">
+    <img src=${item.img} alt=${item.title} class="photo" />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+      ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
   const categoryes = menu.reduce(
     function (values, item) {
       if (!values.includes(item.category)) {
@@ -126,23 +148,4 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
-});
-
-function displayMenuItems(menuItems) {
-  let displayMenu = menuItems.map(function (item) {
-    return ` <article class="menu-item">
-    <img src=${item.img} alt=${item.title} class="photo" />
-    <div class="item-info">
-      <header>
-        <h4>${item.title}</h4>
-        <h4 class="price">$${item.price}</h4>
-      </header>
-      <p class="item-text">
-      ${item.desc}
-      </p>
-    </div>
-  </article>`;
-  });
-  displayMenu = displayMenu.join('');
-  sectionCenter.innerHTML = displayMenu;
 }
