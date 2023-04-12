3f1a6faed438557a9e9c54657648e0173b116bb2
Tanya <taniyhamail@gmail.ua>
2023-04-12 19:56:23 +0300

    10-scroll/added fixed navbar

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -15,14 +15,24 @@ navToggle.addEventListener('click', function () {
   // linksContainer.classList.toggle('show-links');
   const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
-  console.log(linksHeight);
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
   } else {
     linksContainer.style.height = 0;
   }
 });
-// ********** fixed navbar ************
 
+// ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+window.addEventListener('scroll', function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+});
 // ********** smooth scroll ************
 // select links
