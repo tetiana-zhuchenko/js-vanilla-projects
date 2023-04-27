8aac7ec71a29a763c547c91210ad4d91555e8c7e
Tanya <taniyhamail@gmail.ua>
2023-04-27 16:44:21 +0300

    12-countdown-timer/added functionality to the project

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,8 +26,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2024, 4, 24, 11, 30, 0);
-console.log(futureDate);
+let futureDate = new Date(2024, 3, 27, 16, 36, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -35,3 +34,43 @@ const month = futureDate.getMonth();
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${months[month]} ${year} ${hours}:${minutes}`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    return item < 10 ? (item = `0${item}`) : item;
+  }
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(coutndown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
+}
+// coutndown
+let coutndown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
